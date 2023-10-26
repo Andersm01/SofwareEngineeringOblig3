6b487a553f5fd9a8a33166dcbad891ca6522ab25
andersym01 <anders.ym@hotmail.no>
2023-10-26 13:35:23 +0200

    ting

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ def isLeapYear(year):
         return True
     else:
         return False
+
+
+def testIsLeapyearDividedBy400():
+    assert isLeapYear(2000) == True
+
+def test2NotLeapyearSinceNotDividableBy400():
+    assert isLeapYear(1900) == False
+
+def test3IsNotLeapyearSinceNotDividableBy400():
+    assert isLeapYear(1700) == False
+
+def test4IsLeapyearSinceDividableBy4Not100():
+    assert isLeapYear(2020) == True
+
+def test5IsNotLeapyearSinceNotDividableBy4():
+    assert isLeapYear(2100) == False
