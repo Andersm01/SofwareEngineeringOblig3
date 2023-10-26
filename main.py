6a9e7af6253060dad4eaa29e2c0d196a3e611172
Andersm01 <111729349+Andersm01@users.noreply.github.com>
2023-10-26 14:15:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,4 +5,18 @@ def isLeapYear(year):
         return False
 
 
+def testIsLeapyearDividedBy400():
+    assert isLeapYear(2000) == True
+
+def test2NotLeapyearSinceNotDividableBy400():
+    assert isLeapYear(1900) == False
+
+def test3IsNotLeapyearSinceNotDividableBy400():
+    assert isLeapYear(1700) == False
+
+def test4IsLeapyearSinceDividableBy4Not100():
+    assert isLeapYear(2020) == True
+
+def test5IsNotLeapyearSinceNotDividableBy4():
+    assert isLeapYear(2100) == False
 
